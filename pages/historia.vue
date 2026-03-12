ed57abfd6791915638216364760b1f3a5d0acc75
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2026-03-12 12:07:27 -0300

    Add historical context and controversies about Patus Klei

--- pages/historia.vue
+++ pages/historia.vue
@@ -16,13 +16,49 @@
             Con el paso del tiempo fue enviado a China a firmar la paz con Lars Wampiola. El tratado fue celebrado con un panduro y una Crush de Pomelo. Patus vivió sus últimos años en La Mítica Tierra de Cle, donde era conocido por tener un <span class="highlight">topo de mascota llamado RODOLFA MUSCHI KLEI</span>. Fallece en 2007 tras trastabillar mientras se lavaba el pelo en la pileta de la cocina.
           </p>
         </div>
+
+        <h3 class="subtitle">Crónicas y Controversias del Códice XXIII</h3>
+<div class="story-box codex-box">
+  <p class="story-text">
+    <em>Fragmento encontrado en el códice XXIII "Contra Detractis Patus Boletus" del alquimista, detective e historiador proto-gallego Josefo Bachero y Roncá (también conocido como Hyeronimous Bach-Roncaglius, Girolamo da Baccio Roncagatti o Jasper Offenbakken "El Ronco").</em>
+  </p>
+
+  <p class="story-text">
+    ...(Co)mo suele suceder con personajes de la humanidá jumana que han trascendido la misma jistoria de la realidad subyugada, suelen encontrarse diferentes relatos sobre lo acontecido a lo largo de sus recorridos factuales, fácticos, factos factos (que palabras de mierda)... 
+En el caso de nuestro querido Patus Klei, principalmente entre los muchos seguidores, enfermos, devotos, esclavos, enemigos, aduladores y detractores, encontramos variopintos grupos e individuos que intentaron, por un lado, continuar las enseñanzas Patales tratando de ser lo más fieles posible a su palabra, así como también por el otro, los que osaron en reescribir la historia a través de un revisionismo calumniador, infundado y mal intencionado, no solo atacando el menester de nuestro Santo del Bidet, sino también, se han encargado de tergiversar la integridad y honor de Rodolfa Muschi Klei, y como si esto fuera poco, faltarle el respeto a doña Luisana Anna Müschi Plâtze Klei; como es sabido, estos confluyen en dos de los mayores pecados e insultos posibles referidos hacia la investidura Patal-Klecal (si te metes con Rodolfa te metes conmigo, la κόλπος de tu μητέρα).
+  </p>
+
+  <p>He aquí un listado descriptivo de algunos de los aformencionados organismos operantes:/p>
+  <h4 class="codex-item-title">1. Ordis Fraternis Patusmalis (.:.O.:.F.:.P.:.)</h4>
+  <p class="story-text">
+    Escuela de sabiduría y orden de talabartería psíquica, oriunda de las afueras del conurbano profundo Belga. Tomando como base su cosmovisión dualista antinómica no euclidiana, realizó diferentes investigaciones sobre el origen del plano plasmático original oculto bajo la Tierra de Klek, así como también intentaron descifrar el significado de uno de los mayores misterios de nuestra existencia...la mítica e indescriptible "Visión del Bidet Verde" (también conocida como "La Viçón du Le Porè Nevad").
+Se presume sin mucha evidencia e información al respecto, que su principal maestro y fundador es ni más ni menos, que el misterioso Luis C. Wampiolar, veterano de la Batalla de la Triple Panera y uno de los seres más peculiares y poco comprendidos que han merodeado por el inconsciente de los Clekenses.
+Luego de arduas labores, sacrificios y rituales llegaron a la conclusión de la principal teoría que los sustenta: Patus en realidad era Lars Guampiola...y viceversa.
+Esto sacudió la psique de la totalidad de los miembros de la orden debido a la confusión e irracionalidad de dicha premisa, por lo que después de intensos concilios, scismas y cónclaves, sin más preámbulos, procedieron a pactar una Rambosa y se pegaron unos guinches...
+Al día de la fecha solo se conoce un sobreviviente de aquel trágico hecho, un muchacho de apellido Ibn-Gonsáles, al cual no hubo que operarlo de las narpie esta vez, ya que fue el primero en darse cuenta que los <em>plot tuist</em> están quemados y por ende son una mierda, al mismo nivel que decir fáctico (es como el “olvidate” de estas épocas).
+  </p>
+
+  <h4 class="codex-item-title">2. Los Gordos de Klek (Organización con Fines de Lucro Obrero-Sindical Exotérica Neo-Wampiolista s.r.l)</h4>
+  <p class="story-text">
+    Famosos por su ferreo Anti-Rodolfismo y su explícito odio hacia la familia Muschi-Klei, poseen como figura de adoración y líder espiritual a la inflame y siempre controvertida Reina De Clè (reconocida también como Roberta L. Gordblum o simplemente "La Gorda de Clec").
+Expusieron en su obra principal, el panfleto/libelo de sangre intitulado "Nos creemos rebeldes, pero estamos financiados por el barón de Wampiola", todo su resquemor, resentimiento y alevosía frente a la figura Klecal. 
+Entre las innumerables e indescriptibles blasfemias encontradas en dicho opúsculo, se destaca principalmente la grave denuncia efectuada hacia Rodolfa M. Klei, acusandole de haber asesinado furtivamente al mismísimo don Patus, situación que sigue causando revuelo, miradas lascivas, comezones testiculares, y aún más resquemores entre los conciudadanos de TerraClec.
+Su líder, Johannes Karl "El Bébe" Simbronski, fue apresado al encontrarse en sospechosas situaciones luego de efectuarse una orden de allanamiento por falsificación de billetes de 2 mangos moneda corriente. 
+En la escena de la redada, fue encontrado infraganti, repleto de empaques de galletas marca Fauna con fecha de expiración en Setiembre 2010, y con la llamativa y estupefacta compañía de un topo embalsamado, el cual increíblemente presentaba idénticas facciones y contextura física a las de Rodolfa Muschi.  </p>
+
+  <h4 class="codex-item-title">3. Fundación Amigos del Paté</h4>
+  <p class="story-text">
+   Cultores de una vida licenciosa y sin tapujos, parlantes del portugês koiné, convivieron y pernoctaron en grupos de hasta 350 personas en un mínimo espacio de 2x2. Llamativamente alardearon ser practicantes de una dieta 101% a base de paté orgánico, aunque en recientes investigaciones gracias a la honrada Universidad J.C Parlongo, fue descubierto que en verdad verdadera reciclaron sus propias heces en un ciclo sin interrupción, para después ser procesadas como principal y único ingrediente.
+Esta confluencia de seres variados, inusuales y patidifusos, teorizaron sin repetir y sin soplar que Patus en realidad nunca existió física, mental ni espiritualmente. Así tampoco Lars Wampiola, ni mucho menos Rodolfa, y como por asombro ni el mismísimo su excelencia y fundador de la dinastía don Andre Von Cartè Klei...
+Esta premisa de negación total de la realidat, sin ton ni son, los llevó al estrellato y popularidad de manera inesperada. Los noticiarios de TV los aclamaban, los algoritmos promocionaban sus videos de manera nunca antes vista, hasta fueron capaces de conseguir fechas en Holanda para sus conferencias/orgías tántricas masivas.
+Su gurú, poeta maldito, nepotista, e hijo bastardo, conocido coloquialmente por su apodo "Cornalito”, amasó aún más grandes fortunas de las que poseía con su incipiente fama... pero esto no duró demasiado, ya que en pocos meses, el karma Patal hizo de las suyas…mientras regresaba de una reunión para promocionar su nuevo proyecto de paté enlatado, fue interceptado por dos miembros enmascarados de la Guardia Klecal Neo-Patista, los cuales procedieron a secuestrarle. Durante su cautiverio, fue inyectado con un suero manufacturado a base de pastillas de carbón y por consiguiente liberado luego de 12 horas de privación de su libertad. Aunque para algunos esto suena meramente inofensivo, la situación en realidad provocaría fatales e irreparables consecuencias; su posición de líder indiscutible y absoluto en la susodicha agrupación, también significaba incontestablemente ser el primero en la cadena alimenticia de la respectiva e inusual dieta a base del cuestionable paté del cual no volveremos a mencionar como era preparado ni cuál era su ingrediente principal, pero puede deducirse por añadidura que debido a esto fallecieron por inanición todos sus miembros y el mismísimo señor Cornalo.        
+    <em>Fin do Reporte.</em>
+  </p>
+</div>
       </div>
     </section>
   </div>
 </template>
-
-<script setup lang="ts">
-</script>
 
 <style scoped>
 @import '~/assets/css/pages.css';
@@ -31,4 +67,24 @@
   min-height: 100vh;
   padding-top: 100px;
 }
+
+.subtitle {
+  margin-top: 50px;
+  font-family: serif; /* Le da un toque más "códice" */
+  border-bottom: 2px solid #333;
+  padding-bottom: 10px;
+}
+
+.codex-item-title {
+  color: #d4af37; /* Un tono dorado envejecido */
+  margin-top: 25px;
+  margin-bottom: 10px;
+}
+
+.codex-box {
+  background-color: #f9f7f2; /* Color pergamino */
+  border: 1px solid #dcdcdc;
+  padding: 25px;
+  border-radius: 4px;
+}  
 </style>
